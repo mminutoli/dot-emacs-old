42822453f788fe7f6949b058e0048f33690f7040
Marco Minutoli <mminutoli@gmail.com>
2015-12-29 00:03:38 -0800

    Removed projects file.

--- projects.el
+++
@@ -1,6 +0,0 @@
-(when (file-exists-p "~/projects/panda/projects/panda-bambu-wishbone/Makefile.am")
-  (setq cpp-tests-project
-	(ede-cpp-root-project "panda-bambu-wishbone"
-			      :file "~/projects/panda/projects/panda-bambu-wishbone/Makefile.am"
-			      )))
-(provide 'projects)
